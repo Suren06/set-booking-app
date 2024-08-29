440060c9064534ced6a6ba3ebc2c14c58eb39159
Suren06 <suren.boyilla@gmail.com>
2024-08-29 12:49:17 +0800

    add pages & routes

--- setbook-ui/src/App.tsx
+++ setbook-ui/src/App.tsx
@@ -1,8 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Dashboard from "./pages/Dashboard";
 
 const App = () => {
   return (
-    <h1 className="text-3xl text-red-900">Suren</h1>
-  )
-}
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
